b12ebced4bbcde920eacc8e57a2382a441f53a34
jeevapranesh005 <jeevapranesh.vrp@gmail.com>
2026-03-16 16:41:05 +0530

    16-03-26

---
+++ expleoJava/src/Collection/Spliterator_1.java
@@ -0,0 +1,30 @@
+package Collection;
+import java.util.*;
+
+public class Spliterator_1 {
+
+    public static void main(String[] args) {
+
+        ArrayList<Double> arr = new ArrayList<>();
+
+        System.out.println("Size : " + arr.size());
+
+        arr.add(1.0);
+        arr.add(2.0);
+        arr.add(3.0);
+        arr.add(4.0);
+        arr.add(5.0);
+        arr.add(6.0);
+
+        System.out.println("Contents of ArrayList using tryAdvance");
+
+        Spliterator<Double> sitr = arr.spliterator();
+        while(sitr.tryAdvance(n -> System.out.println(n + " ")));
+        System.out.println();
+        System.out.println("Contents of ArrayList using forEachRemaining");
+        sitr = arr.spliterator();
+        sitr.forEachRemaining(n -> System.out.println(n + " "));
+        System.out.println();
+        System.out.println("Size of ArrayList after insertion is : " + arr.size());
+    }
+}
