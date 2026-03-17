8b44f9b7034bd3e2fe9b75e1d3e1f6c1dbcfc8e8
jeevapranesh005 <jeevapranesh.vrp@gmail.com>
2026-03-17 09:09:37 +0530

    17-03-26

--- expleoJava/src/Collection/Spliterator_1.java
+++ expleoJava/src/Collection/Spliterator_1.java
@@ -6,9 +6,7 @@ public class Spliterator_1 {
     public static void main(String[] args) {
 
         ArrayList<Double> arr = new ArrayList<>();
-
         System.out.println("Size : " + arr.size());
-
         arr.add(1.0);
         arr.add(2.0);
         arr.add(3.0);
@@ -16,8 +14,8 @@ public class Spliterator_1 {
         arr.add(5.0);
         arr.add(6.0);
 
+   
         System.out.println("Contents of ArrayList using tryAdvance");
-
         Spliterator<Double> sitr = arr.spliterator();
         while(sitr.tryAdvance(n -> System.out.println(n + " ")));
         System.out.println();
